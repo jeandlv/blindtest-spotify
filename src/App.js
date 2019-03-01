7e9d07e1e58779ddf79aac0f5b684a50cd2d93a7
jeandlv <Jean@localhost.localdomain>
2019-03-01 16:21:46 +0100

    Get tracks from spotify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+const apiToken = 'BQACA64fs0s8V6H7My8QaMrFcmCMp0PSmnSOfcVSkValrWx3PJ9IF9T1sOaMB6ZAUd_5yTsaNjVaxySe3OR74o2gpcWQruS8FFx398f4LLR--DZd6YT6q_YMU-DqXJIfdMgvJI-HrcOTnrzp2X8ytzLm';
+var tracks;
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -41,6 +42,20 @@ class App extends Component {
     this.setState({
       text: "Bonjour"
     });
+    fetch('https://api.spotify.com/v1/me/tracks', {
+      method: 'GET',
+      headers: {
+       Authorization: 'Bearer ' + apiToken,
+      },
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log("Réponse reçue ! Voilà ce que j'ai reçu : ", data);
+      this.setState({
+        text: "Your library tracks were well loaded"
+      });
+      tracks = data.items;
+    })
   }
 
   render() {
